0418e12412116c5b534a170c52650f03d1ed692e
nam1224 <0217minu@gmail.com>
2023-04-30 18:30:17 +0900

    player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,20 +6,34 @@ public class Player : MonoBehaviour
 {
     public GameObject player;
     private Rigidbody2D rb2D;
-    public float movementSpeed = 5f;
+    public float power = 350;
+    bool isJumping = true;
 
     void Jump()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Click Complete");
-            rb2D.AddForce(transform.up * 100);
-            //player.transform.position = new Vector2(player.transform.position.x, movementSpeed * Time.deltaTime);
+            if (isJumping == false)
+            {
+                Debug.Log("Click Complete");
+                rb2D.AddForce(transform.up * power);
+                //player.transform.position = new Vector2(player.transform.position.x, movementSpeed * Time.deltaTime);
+                isJumping = true;
+            }
+            else return;
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("충돌 중");
+        if (collision.gameObject.CompareTag("Land"))
+            isJumping = false;
+    }
+
     private void Start()
     {
-        rb2D = GetComponent<Rigidbody2D>();
+        rb2D = GetComponent<Rigidbody2D>(); //script를 가진 object의 Rigidbody를 가져옴
     }
 
 
