ebafca6061e953aa353ae89bcf502a433323a839
nam1224 <105091319+nam1224@users.noreply.github.com>
2023-05-11 13:49:38 +0900

    05.11_최종

--- Assets/Scripts/RepeatBg.cs
+++ Assets/Scripts/RepeatBg.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class RepeatBg : MonoBehaviour
 {
-    public GameObject bg1;
+    [SerializeField] [Range(1f, 20f)] float speed = 3f;
+
+    [SerializeField] float posValue;
+
+    Vector2 startPos;
+    float newPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        newPos = Mathf.Repeat(Time.time * speed, posValue);
+        transform.position = startPos * Vector2.right * newPos;
+    }
 }
